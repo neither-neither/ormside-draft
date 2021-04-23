8edab1e0d475c48defab837889b25e7430ac73af
Ben Walker <ben.walker.1212@gmail.com>
2021-04-23 11:17:24 +0100

    content tweaks

---
+++ js/tracklist.js
@@ -0,0 +1,7 @@
+const tracklistLink = document.querySelector(".tracklist-link");
+
+tracklistLink.addEventListener("click", () => {
+  document.querySelector(".tracklist").style.display = "block";
+});
+
+console.log("hello");
