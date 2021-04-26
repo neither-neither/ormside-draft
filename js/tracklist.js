ff3736ce932fcdaf7e5149c90a35a13fb02f2e33
Ben Walker <ben.walker.1212@gmail.com>
2021-04-26 08:52:14 +0100

    changes before v3 goes live - new content, image orig sizes

--- js/tracklist.js
+++ js/tracklist.js
@@ -2,6 +2,7 @@ const tracklistLink = document.querySelector(".tracklist-link");
 
 tracklistLink.addEventListener("click", () => {
   document.querySelector(".tracklist").style.display = "block";
+  document.querySelector(".tracklist-link").innerText = "Tracklist:";
 });
 
 console.log("hello");
